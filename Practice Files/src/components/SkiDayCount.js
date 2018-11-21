858ed3b97551f2f3b0fbb7f8f96fc06643452479
Sachit <sachit.shetty@gmail.com>
2018-11-21 00:18:15 -0500

    react practice ch3

---
+++ Practice Files/src/components/SkiDayCount.js
@@ -0,0 +1,38 @@
+
+import '../stylesheets/ui.scss'
+import { IoIosRocket } from 'react-icons/io'
+import { IoMdSnow } from 'react-icons/io';
+import { IoIosCalendar } from 'react-icons/io';
+
+const percentToDecimal = (decimal) => {
+    return ((decimal * 100) + '%')
+}
+
+const calcGoalProgress = (total,goal) => {
+    return percentToDecimal(total/goal)
+}
+
+export const SkiDayCount = ({total, powder, backcountry, goal}) => (
+    <div className="ski-day-count">
+        <div className= "total-days">
+            <span>{total}</span>
+                <IoIosCalendar />
+            <span>days</span>
+        </div>
+        <div className="powder-days">
+            <span>{powder}</span>
+                <IoMdSnow />
+            <span>days</span>
+        </div>
+        <div className="backcountry-days">
+            <span>{backcountry}</span>
+                <IoIosRocket />
+            <span>days</span>
+        </div>
+        <div>
+            <span>
+            {calcGoalProgress(total, goal)}
+            </span>
+        </div>
+    </div>
+)
--- Practice Files/webpack.config.js
+++ Practice Files/webpack.config.js
@@ -22,7 +22,21 @@ module.exports = {
                         query: {
                             presets: ["env", "stage-0", "react"]
                         }
+                    },
+                    {
+                        test: /\.json$/,
+                        exclude: /(node_modules)/,
+                        loader: "json-loader"
+                    },
+                    {
+                        test: /\.css$/,
+                        loader: 'style-loader!css-loader!autoprefixer-loader'
+                    },
+                    {
+                        test: /\.scss$/,
+                        loader: 'style-loader!css-loader!autoprefixer-loader!sass-loader'
                     }
+
                 ]
         }
 }
