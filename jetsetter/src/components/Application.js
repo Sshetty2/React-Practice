45abb6c198e970a6b8f55c1437eae52e0d929bb9
Sachit <sachit.shetty@gmail.com>
2019-01-30 21:40:36 -0500

    practice

--- jetsetter/src/components/Application.js
+++ jetsetter/src/components/Application.js
@@ -16,14 +16,26 @@ const defaultState = [
   { value: 'Hat', id: uniqueId(), packed: false },
   { value: 'T-Shirts', id: uniqueId(), packed: false },
   { value: 'Belt', id: uniqueId(), packed: false },
-  { value: 'Passport', id: uniqueId(), packed: true },
+  { value: 'Passport', id: uniqueId(), packed: false },
   { value: 'Sandwich', id: uniqueId(), packed: true },
 ];
 
 class Application extends Component {
   state = {
-    // Set the initial state,
+    items: defaultState
   };
+
+ addItem = (item) =>{
+    this.setState({
+      items: [item, ...this.state.items]
+    })
+  }
+
+  removeItem = (itemToRemove) =>{
+    this.setState({
+      items: this.state.items.filter(item => item.id !== itemToRemove.id)
+    })
+  }
 
   // How are we going to manipulate the state?
   // Ideally, users are going to want to add, remove,
@@ -31,13 +43,17 @@ class Application extends Component {
 
   render() {
     // Get the items from state
+    const { items } = this.state
+    const packedItems = items.filter(item => item.packed)
+    const unpackedItems = items.filter(item => !item.packed)
+
 
     return (
       <div className="Application">
-        <NewItem />
+        <NewItem onSubmit={this.addItem.bind(this)} />
         <CountDown />
-        <Items title="Unpacked Items" items={[]} />
-        <Items title="Packed Items" items={[]} />
+        <Items title="Unpacked Items" items={unpackedItems} onRemove={this.removeItem} />
+        <Items title="Packed Items" items = {packedItems} onRemove={this.removeItem} />
         <button className="button full-width">Mark All As Unpacked</button>
       </div>
     );
--- jetsetter/src/components/NewItem.js
+++ jetsetter/src/components/NewItem.js
@@ -7,7 +7,8 @@ class NewItem extends Component {
   state = { value: '' };
 
   handleChange = event => {
-    // Do something when the state of this input changes.
+    const value = event.target.value
+    this.setState({ value })
   };
 
   handleSubmit = event => {
@@ -15,10 +16,9 @@ class NewItem extends Component {
     const { value } = this.state;
 
     event.preventDefault();
+    onSubmit({value, id: uniqueId, packed:false });
+    this.setState({ value : ""})
 
-    // Do something when a new value is submitted.
-
-    // Reset the state of the component.
   };
 
   render() {
